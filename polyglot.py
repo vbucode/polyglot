f7df267c320d510e1c3d848a08ed3a17bfd986c2
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-16 18:30:29 +0500

    Add files via upload

---
+++ polyglot.py
@@ -0,0 +1,57 @@
+import random
+import re
+import json
+
+data = """{"i will not travel in winter":"я не буду путешествовать зимой",
+     "what are you doing next week?":"что ты делаешь на следующей неделе?",
+     "what do you think?":"что скажешь?",
+     "how long have you know her?":"как давно вы с ней знакомы?",
+     "is this your cellphone?":"это твой телефон?"}"""
+
+listforhelp = ["i you he she we they it",
+               "am are is",
+               "be was were",
+               "have has had",
+               "do did does",
+               "this that their"]
+
+d = json.loads(data)
+
+def main():
+    wl = []
+    count = 0
+    engvar, ruvar = random.choice(list(d.items()))
+    print("\n{}\n".format(ruvar))
+    ispl = spliting(engvar)
+    for i in ispl:
+        for j in listforhelp:
+            x = j.split(" ")
+            tlist = []
+            if i in x:
+                c = 2
+                tlist.append(i)
+                while c > 0:
+                    y = random.choice(x)
+                    if y not in tlist:
+                        tlist.append(y)
+                        c -= 1
+                print(tlist)
+    tr = input("\ntranslate: ")
+    if tr == "exit":
+        leave()
+    wl = spliting(tr)
+    for i in wl:
+        count += 1
+        if i != ispl[count-1]:
+            print("\nright answer: {}".format(engvar))
+            break
+        elif count == len(wl):
+            print("\ngood!")
+def spliting(text):
+    wlist = []
+    wlist = re.split("[\W\s]", text)
+    return wlist
+def leave():
+    exit()
+while True:
+    main()
