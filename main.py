3e109c18bdb9274095bc2593e6bf715f232bee86
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-21 03:27:17 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ root = tkinter.Tk()
 root.title("Polyglot")
 root.geometry("430x180")
 root.configure(bg = "black")
+flag = 0
 
 def chek():
     global po
@@ -19,12 +20,13 @@ def chek():
         count += 1
         if i != pol[count-1]:
             txt = "right answer: " + po[0]
-            lbl3.configure(text = txt)
+            lbl3.configure(text = ent.get())
+            lbl4.configure(text = txt)
             break
         elif count == len(wl):
-            lbl3.configure(text = "Good!")
+            lbl3.configure(text = po[0])
+            lbl4.configure(text = "Good!")
     ent.delete(0, "end")
-    step()
 
 def step():
     global po
@@ -35,21 +37,36 @@ def step():
     pts = " ".join(pt)
     lbl1.configure(text = pos)
     lbl2.configure(text = pts)
+    lbl3.configure(text = "")
+    lbl4.configure(text = "")
+
+def start():
+    global flag
+    if flag == 0:
+        btn1["text"] = "enter"
+        flag = 1
+        step()
+    else:
+        btn1["text"] = "next"
+        chek()
+        flag = 0
 
 lbl1 = tkinter.Label(root, text = "", fg = "yellow", bg = "black")
 lbl2 = tkinter.Label(root, text = "", fg = "yellow", bg = "black")
 lbl3 = tkinter.Label(root, text = "", fg = "yellow", bg = "black")
+lbl4 = tkinter.Label(root, text = "", fg = "yellow", bg = "black")
 ent = tkinter.Entry(root, width = 40)
 ent.focus()
-btn1 = tkinter.Button(root, text = "next", bg = "gray", command = chek)
+btn1 = tkinter.Button(root, text = "enter", bg = "gray", command = start)
 
 lbl1.pack()
 lbl2.pack()
 lbl3.pack()
+lbl4.pack()
 ent.place(x = 15, y = 100 )
 btn1.place(x = 350, y = 96)
 
-step()
+start()
 
 if __name__ == "__main__":
     root.mainloop()
